d3ea95bcf9b9b160d83b6c9f3786c5aa091033d5
qijia.wang@renren-inc.com <qijia.wang@renren-inc.com>
2019-09-17 13:53:42 +0800

    提交

---
+++ src/main/java/com/renren/log/test/LogTest.java
@@ -0,0 +1,19 @@
+package com.renren.log.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LogTest {
+    private final static Logger logger =  LoggerFactory.getLogger(LogTest.class);
+
+    public static void main(String[] args) {
+
+
+        logger.error("错误");
+
+
+
+
+
+    }
+}
